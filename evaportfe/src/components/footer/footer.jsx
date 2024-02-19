04f6a94250c52b9506fcc334261602b45c4e09a7
VuranokYigit <fyvuranokk@gmail.com>
2024-02-19 14:12:18 +0300

    publishedEvaPortOpenLinkNewTab

--- evaportfe/src/components/footer/footer.jsx
+++ evaportfe/src/components/footer/footer.jsx
@@ -48,10 +48,10 @@ const Footer = () => {
                         <div style={{textTransform:"capitalize"}} className="col-6 col-md-4 mb-4 mb-md-0" id="footerContactSection">
                             <h3>{t('footerContactTitle')}</h3>
                             <div className="footer-social" id="footersocial">
-                                <a href="https://www.instagram.com/evaportlda/" id="insta">
+                                <a href="https://www.instagram.com/evaportlda/" id="insta" target="_blank">
                                    <span><FontAwesomeIcon icon={faInstagram} size="xl"/></span>
                                 </a>
-                                <a href="https://www.linkedin.com/company/evaport/" id="linked">
+                                <a href="https://www.linkedin.com/company/evaport/" id="linked" target="_blank">
                                    <span> <FontAwesomeIcon icon={faLinkedin} size="xl" /></span>
                                 </a>
                             </div>
